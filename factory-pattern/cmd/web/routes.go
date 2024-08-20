6681777338e998b6d27fa3427cdba1fa2e87352c
Sayed-Imran <sayedimran00786@gmail.com>
2024-08-20 22:42:25 +0530

    Factory Pattern

---
+++ factory-pattern/cmd/web/routes.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Timeout(60 * time.Second))
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	//display our test page
+	mux.Get("/test-patterns", app.TestPatterns)
+
+	// factory routes
+	mux.Get("/api/dog-from-factory", app.CreateDogFromFactory)
+	mux.Get("/api/cat-from-factory", app.CreateCatFromFactory)
+
+	mux.Get("/", app.ShowHome)
+	mux.Get("/{page}", app.ShowPage)
+
+	return mux
+}
